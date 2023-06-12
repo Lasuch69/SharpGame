896f3ad3b5cd2b48ef85fde2ada7126354671894
Lasuch <stylizedstickman@gmail.com>
2023-06-12 12:22:54 +0200

    Move spawning system to Game singleton

--- scripts/Level.cs
+++ scripts/Level.cs
@@ -1,35 +1,13 @@
 using Godot;
 using System;
-using System.Collections.Generic;
 
 public partial class Level : Node2D
 {
 	[Export]
-	private NavigationRegion2D _navigationRegion;
-
-	[Export]
-	private Timer _spawnTimer;
-
-	[Export]
-	private PackedScene _enemy;
-
-	private Game _game;
+	public NavigationRegion2D NavigationMap;
 
 	public override void _Ready()
 	{
-		_spawnTimer.Timeout += OnSpawnTimerTimeout;
-		_game = GetNode<Game>("/root/Game");
-	}
-
-	private void OnSpawnTimerTimeout()
-	{
-		Vector2[] verticies = _navigationRegion.NavigationPolygon.GetOutline(0);
-		int[] indices = Geometry2D.TriangulatePolygon(verticies);
-
-		Vector2 spawnPoint = _game.GetSpawnPoint(verticies, indices);
-
-		Node2D enemy = (Node2D)_enemy.Instantiate();
-		enemy.Position = spawnPoint;
-		GetTree().CurrentScene.AddChild(enemy);
+		GetNode<Game>("/root/Game").SetLevel(this);
 	}
 }
--- scripts/singletons/Game.cs
+++ scripts/singletons/Game.cs
@@ -34,13 +34,61 @@ public partial class Game : Node
 	private int _score;
 
 	private Geometry _geometry;
+	private Level _level;
+	private Timer _spawnTimer;
 
 	public override void _Ready()
 	{
-		_geometry = new Geometry();
+		_geometry = new ();
+		_spawnTimer = new ();
+
+		_spawnTimer.WaitTime = 1.0f;
+		_spawnTimer.ProcessCallback = Timer.TimerProcessCallback.Physics;
+
+		_spawnTimer.Timeout += OnSpawnTimerTimeout;
+
+		AddChild(_spawnTimer);
 	}
 
-	public Vector2 GetSpawnPoint(Vector2[] verticies, int[] indices, float desiredDistance = 128.0f, int iterations = 16)
+	private void OnSpawnTimerTimeout()
+	{
+		if (_level == null)
+		{
+			_spawnTimer.Stop();
+			return;
+		}
+
+		if (_level.NavigationMap == null)
+		{
+			_spawnTimer.Stop();
+			return;
+		}
+
+		Vector2[] verticies = _level.NavigationMap.NavigationPolygon.GetOutline(0);
+		int[] indices = Geometry2D.TriangulatePolygon(verticies);
+
+		Vector2 spawnPoint = GetSpawnPoint(verticies, indices, 256.0f);
+		
+		PackedScene enemyScene = (PackedScene)GD.Load("res://scenes/flyer.tscn");
+		Node2D enemy = (Node2D)enemyScene.Instantiate();
+		enemy.Position = spawnPoint;
+		
+		GetTree().CurrentScene.AddChild(enemy);
+	}
+
+	public void SetLevel(Level level)
+	{
+		_level = level;
+		_spawnTimer.Start();
+	}
+
+	private float CalculateCost(Vector2 point, Vector2 target, float desiredDistance)
+	{
+		float distance = point.DistanceTo(target);
+		return Mathf.Abs(distance - desiredDistance);
+	}
+
+	private Vector2 GetSpawnPoint(Vector2[] verticies, int[] indices, float desiredDistance = 128.0f, int iterations = 16)
 	{
 		Vector2[] points = _geometry.GetRandomPointsInPolygon(verticies, indices, iterations);
 
@@ -66,9 +114,5 @@ public partial class Game : Node
 		return points[lowestCostIndex];
 	}
 
-	private float CalculateCost(Vector2 point, Vector2 target, float desiredDistance)
-	{
-		float distance = point.DistanceTo(target);
-		return Mathf.Abs(distance - desiredDistance);
-	}
+	
 }
