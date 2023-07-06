bd25d7625c487dab4bf2c7039d35d8f552f3f846
Lasuch <stylizedstickman@gmail.com>
2023-07-06 17:24:42 +0200

    Update Navigation singleton

--- scripts/singletons/Navigation.cs
+++ scripts/singletons/Navigation.cs
@@ -4,7 +4,6 @@ using System;
 public partial class Navigation : Node
 {
 	private AStarGrid2D _aStar = new ();
-	private Vector2I _offset = new ();
 
 	public override void _Ready()
 	{
@@ -12,25 +11,18 @@ public partial class Navigation : Node
 		_aStar.JumpingEnabled = true;
 	}
 
-	public Vector2 GetPosition(Vector2I id)
-	{
-		return (id * _aStar.CellSize) + _aStar.CellSize / 2;
-	}
-
-	public Vector2I GetId(Vector2 position)
+	private Vector2I GetId(Vector2 position)
 	{
 		return (Vector2I)((position - _aStar.CellSize / 2) / _aStar.CellSize).Round();
 	}
 
 	public Godot.Collections.Array<Vector2> GetPath(Vector2 from, Vector2 to)
 	{
-		var pathId = GetIdPath(GetId(from), GetId(to));
 		Godot.Collections.Array<Vector2> path = new ();
-		path.Resize(pathId.Count);
-
-		for (int i = 0; i < path.Count; i++)
+		
+		foreach (Vector2 point in _aStar.GetPointPath(GetId(from), GetId(to)))
 		{
-			path[i] = GetPosition(pathId[i]);
+			path.Add(point);
 		}
 
 		return path;
@@ -38,42 +30,21 @@ public partial class Navigation : Node
 
 	public Godot.Collections.Array<Vector2I> GetIdPath(Vector2I fromId, Vector2I toId)
 	{
-		var path = _aStar.GetIdPath(ToLocal(fromId), ToLocal(toId));
-
-		for (int i = 0; i < path.Count; i++)
-		{
-			path[i] = ToGlobal(path[i]);
-		}
-
-		return path;
+		return _aStar.GetIdPath(fromId, toId);
 	}
 
 	public void SetTileMap(TileMap tileMap)
 	{
-		Rect2I map = tileMap.GetUsedRect();	
-		int cellSize = tileMap.CellQuadrantSize;
+		Vector2 cellSize = new (tileMap.CellQuadrantSize, tileMap.CellQuadrantSize);
 
-		_aStar.CellSize = new Vector2(cellSize, cellSize);
-		_aStar.Size = map.Size;
+		_aStar.CellSize = cellSize;
+		_aStar.Offset = cellSize / 2; // Make point the center of a cell
+		_aStar.Region = tileMap.GetUsedRect();
 		_aStar.Update();
-		
-		_offset = map.Position;
 
-		Godot.Collections.Array<Vector2I> usedCells = tileMap.GetUsedCells(0);
-
-		foreach (Vector2I cell in usedCells)
+		foreach (Vector2I cell in tileMap.GetUsedCells(0))
 		{
-			_aStar.SetPointSolid(ToLocal(cell), true);
+			_aStar.SetPointSolid(cell, true);
 		}
-	}
-
-	private Vector2I ToLocal(Vector2I id)
-	{
-		return id - _offset;
-	}
-
-	private Vector2I ToGlobal(Vector2I id)
-	{
-		return id + _offset;
 	}
 }
