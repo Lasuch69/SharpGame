95da67a8752e50d85ce9d22a64853b47b0c5ecbf
Lasuch <stylizedstickman@gmail.com>
2023-07-07 23:16:50 +0200

    Move spawner logic from singleton to node

--- scripts/Spawner.cs
+++ scripts/Spawner.cs
@@ -5,11 +5,24 @@ public partial class Spawner : Node
 {
 	private Game _game;
 	private Godot.Collections.Array<Marker2D> _markers = new ();
+	
+	public int Wave { get => _wave; }
+
+	private int _score = 0;
+	private int _wave = 0;
+	
+	private Player _player = null;
+
+	private Godot.Collections.Array<PackedScene> _spawnQueue = new ();
+	private Godot.Collections.Array<Node> _entities = new ();
+
+	private Timer _timer = new ();
+
+	private PackedScene _enemyScene = (PackedScene)GD.Load("res://scenes/flyer.tscn");
 
 	public override void _Ready()
 	{
 		_game = GetNode<Game>("/root/Game");
-		_game.Spawner = this;
 		
 		foreach (Node node in GetChildren())
 		{
@@ -18,6 +31,37 @@ public partial class Spawner : Node
 
 			_markers.Add((Marker2D)node);
 		}
+		
+		_timer.WaitTime = 2.0f;
+		_timer.ProcessCallback = Timer.TimerProcessCallback.Physics;
+		_timer.Timeout += OnTimerTimeout;
+
+		AddChild(_timer);
+
+		StartWave();
+	}
+
+	private void OnTimerTimeout()
+	{
+		if (_spawnQueue.Count == 0)
+		{
+			_timer.Stop();
+			return;
+		}
+
+		int idx = _spawnQueue.Count - 1;
+		PackedScene scene = _spawnQueue[idx];
+		_spawnQueue.RemoveAt(idx);
+		
+		Node2D entity = Spawn(scene);
+
+		if (entity == null)
+			return;
+		
+		entity.GetNode<HealthComponent>("HealthComponent").HealthEmpty += (instigator) => 
+			OnEntityKilled(entity);
+		
+		_entities.Add(entity);
 	}
 
 	public Node2D Spawn(PackedScene scene)
@@ -49,6 +93,53 @@ public partial class Spawner : Node
 		GetTree().CurrentScene.AddChild(entity);
 
 		return entity;
+	}
+
+	private void OnEntityKilled(Node2D entity)
+	{
+		_entities.Remove(entity);
+
+		if (_spawnQueue.Count != 0 || _entities.Count != 0)
+			return;
+
+		FinishWave();
+		StartWave();
+	}
+
+	private void StartWave()
+	{
+		_wave++;
+		GD.Print("Wave: ", _wave);
+
+		_spawnQueue = GenerateSpawnQueue(_wave);
+
+		_timer.Start();
+	}
+
+	private void FinishWave()
+	{
+		GD.Print("Wave finished!");
+	}
+
+	private int GetWaveThreatPoints(int wave)
+	{
+		double value = ((wave - 1) % 5) + (wave / 5) + 1;
+		value *= value;
+
+		return (int)value;
+	}
+	
+	private Godot.Collections.Array<PackedScene> GenerateSpawnQueue(int wave)
+	{
+		int threatPoints = GetWaveThreatPoints(wave);
+		Godot.Collections.Array<PackedScene> queue = new ();
+
+		for (int i = 0; i < threatPoints; i++)
+		{
+			queue.Add(_enemyScene);
+		}
+
+		return queue;
 	}
 
 	private float CalculateCost(Vector2 from, Vector2 to, float desiredDistance = 128.0f)
--- scripts/singletons/Game.cs
+++ scripts/singletons/Game.cs
@@ -1,6 +1,5 @@
 using Godot;
 using System;
-using System.Collections.Generic;
 
 public partial class Game : Node
 {
@@ -9,12 +8,6 @@ public partial class Game : Node
 
 	[Signal]
 	public delegate void ScoreChangedEventHandler(int score);
-
-	[Signal]
-	public delegate void WaveStartedEventHandler(int wave);
-
-	[Signal]
-	public delegate void WaveFinishedEventHandler(int wave);
 
 	public Player Player
 	{
@@ -38,115 +31,10 @@ public partial class Game : Node
 				return;
 			
 			_score = value;
-		   	EmitSignal(SignalName.ScoreChanged, _score);
+			EmitSignal(SignalName.ScoreChanged, _score);
 		}
 	}
-
-	public Spawner Spawner { 
-		get => _spawner; 
-		set
-		{
-			_spawner = value;
-			
-			if (_spawner != null)
-				StartWave();
-		}
-	}
-
-	public int Wave { get => _wave; }
 
 	private int _score = 0;
-	private int _wave = 0;
-	
 	private Player _player = null;
-	private Spawner _spawner = null;
-
-	private List<PackedScene> _spawnQueue = new ();
-	private List<Node> _entities = new ();
-
-	private Timer _timer = new ();
-
-	private PackedScene _enemyScene = (PackedScene)GD.Load("res://scenes/flyer.tscn");
-
-	public override void _Ready()
-	{
-		_timer.WaitTime = 2.0f;
-		_timer.ProcessCallback = Timer.TimerProcessCallback.Physics;
-		_timer.Timeout += OnTimerTimeout;
-
-		AddChild(_timer);
-	}
-
-	private void OnTimerTimeout()
-	{
-		if (_spawnQueue.Count == 0)
-		{
-			_timer.Stop();
-			return;
-		}
-
-		int idx = _spawnQueue.Count - 1;
-		PackedScene scene = _spawnQueue[idx];
-		_spawnQueue.RemoveAt(idx);
-		
-		Node2D entity = Spawner.Spawn(scene);
-
-		if (entity == null)
-			return;
-		
-		entity.GetNode<HealthComponent>("HealthComponent").HealthEmpty += (instigator) => 
-			OnEntityKilled(entity);
-		
-		_entities.Add(entity);
-	}
-
-	private void OnEntityKilled(Node2D entity)
-	{
-		_entities.Remove(entity);
-
-		if (_spawnQueue.Count != 0 || _entities.Count != 0)
-			return;
-
-		FinishWave();
-		StartWave();
-	}
-
-	private void StartWave()
-	{
-		_wave++;
-		GD.Print("Wave: ", _wave);
-
-		_spawnQueue = GenerateSpawnQueue(_wave);
-
-		_timer.Start();
-
-		EmitSignal(SignalName.WaveStarted, _wave);
-	}
-
-	private void FinishWave()
-	{
-		GD.Print("Wave finished!");
-		EmitSignal(SignalName.WaveFinished, _wave);
-	}
-
-	private int GetWaveThreatPoints(int wave)
-	{
-		double value = ((wave - 1) % 5) + (wave / 5) + 1;
-		value *= value;
-
-		return (int)value;
-	}
-	
-	private List<PackedScene> GenerateSpawnQueue(int wave)
-	{
-		int threatPoints = GetWaveThreatPoints(wave);
-		List<PackedScene> queue = new ();
-
-		for (int i = 0; i < threatPoints; i++)
-		{
-			queue.Add(_enemyScene);
-		}
-
-		return queue;
-	}
 }
