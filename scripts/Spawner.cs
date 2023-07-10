7ca7acf97387998cc4f9f17e8331cf540581e32c
Lasuch <stylizedstickman@gmail.com>
2023-07-10 22:28:14 +0200

    Reimplement wave system

--- scripts/Spawner.cs
+++ scripts/Spawner.cs
@@ -3,6 +3,9 @@ using System;
 
 public partial class Spawner : Node2D
 {
+	[Signal]
+	public delegate void WaveFinishedEventHandler();
+
 	[Export]
 	private Player _player = null;
 
@@ -11,19 +14,66 @@ public partial class Spawner : Node2D
 
 	private PhysicsDirectSpaceState2D _spaceState = null;
 	private PackedScene _enemyScene = (PackedScene)GD.Load("res://scenes/flyer.tscn");
-	
 	private Random _random = new ();
+
+	private Godot.Collections.Array<PackedScene> _queue = new ();
+	private Godot.Collections.Array<Node2D> _entities = new ();
 	
 	public override void _Ready()
 	{
-		_timer.Timeout += () => Spawn(_enemyScene);
-		_timer.Start();
+		_timer.Timeout += OnTimerTimeout;
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
 		if (_spaceState == null)
 			_spaceState = PhysicsServer2D.SpaceGetDirectState(GetWorld2D().Space);
+	}
+
+	private void OnTimerTimeout()
+	{
+		if (_queue.Count == 0)
+		{
+			_timer.Stop();
+			return;
+		}
+
+		int last = _queue.Count - 1;
+		PackedScene scene = _queue[last];
+		_queue.RemoveAt(last);
+
+		Node2D entity = Spawn(scene);
+		_entities.Add(entity);
+
+		entity.TreeExiting += () =>
+		{
+			_entities.Remove(entity);
+
+			if (_entities.Count == 0 && _queue.Count == 0)
+				EmitSignal(SignalName.WaveFinished);
+		};
+	}
+
+	public void StartWave(int wave)
+	{
+		int points = Mathf.Clamp(wave, 1, int.MaxValue);
+
+		_queue.Resize(points);
+		_queue.Fill(_enemyScene);
+		
+		_timer.Start();
+	}
+
+	private Vector2 GetRandomPoint(float desiredDistance)
+	{
+		float a = _random.NextSingle() * 2 * MathF.PI;
+
+		float i = _random.NextSingle() * 0.25f;
+		i -= i / 2.0f;
+
+		float r = desiredDistance * (1.0f + i);
+
+		return new Vector2(r * MathF.Cos(a), r * MathF.Sin(a));
 	}
 
 	private Node2D Spawn(PackedScene scene)
@@ -66,17 +116,5 @@ public partial class Spawner : Node2D
 		GetParent().AddChild(entity);
 
 		return entity;
-	}
-
-	private Vector2 GetRandomPoint(float desiredDistance)
-	{
-		float a = _random.NextSingle() * 2 * MathF.PI;
-
-		float i = _random.NextSingle() * 0.25f;
-		i -= i / 2.0f;
-
-		float r = desiredDistance * (1.0f + i);
-
-		return new Vector2(r * MathF.Cos(a), r * MathF.Sin(a));
 	}
 }
---
+++ scripts/World.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+
+public partial class World : Node
+{
+	[Signal]
+	public delegate void WaveStartedEventHandler(int wave);
+
+	public int Wave { get => _wave; }
+
+	private int _wave = 1;
+
+	[Export]
+	private Spawner _spawner = null;
+
+	public override void _Ready()
+	{
+		_spawner.StartWave(_wave);
+
+		_spawner.WaveFinished += () =>
+		{
+			_wave++;
+			_spawner.StartWave(_wave);
+		};
+	}
+}
