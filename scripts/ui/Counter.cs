779309df0aef985ccebd91aa60eb21c442468bcb
valkyrienyanko <sebastianbelle074@protonmail.com>
2023-07-23 14:22:57 -0500

    convert to local function

--- scripts/ui/Counter.cs
+++ scripts/ui/Counter.cs
@@ -14,11 +14,8 @@ public partial class Counter : Label
 		int timeSeconds = (int)((Time.GetTicksMsec() - timestamp) / 1000);
 		int timeMinutes = timeSeconds / 60;
 
-		Func<int, string> Format = (time) =>
-		{
-			return String.Format("{0, 0:D2}", time);
-		};
+        string Format(int time) => String.Format("{0, 0:D2}", time);
 
-		return String.Format("{0}:{1}", Format(timeMinutes), Format(timeSeconds % 60));
+        return String.Format("{0}:{1}", Format(timeMinutes), Format(timeSeconds % 60));
 	}
 }
