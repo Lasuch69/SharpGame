5df512370d02af08ea50c673e94948f72c663471
Lasuch <stylizedstickman@gmail.com>
2023-08-08 00:27:11 +0200

    Remove unused AnimationPlayer in Player scene

--- scripts/entities/Player.cs
+++ scripts/entities/Player.cs
@@ -9,27 +9,13 @@ public sealed partial class Player : CharacterBody2D
     [Export]
     public PackedScene Projectile;
 
-    [Export]
-    AnimationPlayer AnimationPlayer;
-
     public override void _Ready()
     {
         Game game = GetNode<Game>("/root/Game");
         game.Player = this;
-
-        HealthComponent.HealthChanged += (newHealth, oldHealth) =>
-        {
-            if (newHealth < oldHealth)
-                AnimationPlayer.Play("damaged");
-        };
     }
 
     public override void _Input(InputEvent @event)
-    {
-        Shoot(@event);
-    }
-
-    void Shoot(InputEvent @event)
     {
         if (!@event.IsActionPressed("shoot"))
             return;
@@ -37,9 +23,10 @@ public sealed partial class Player : CharacterBody2D
         var projectile = (Projectile)Projectile.Instantiate();
 
         Vector2 mousePosition = GetGlobalMousePosition();
+        Vector2 direction = Position.DirectionTo(mousePosition);
 
-        projectile.Position = Position.DirectionTo(mousePosition) * 12.0f + Position;
-        projectile.Velocity = Position.DirectionTo(mousePosition) * 256.0f;
+        projectile.Position = direction * 12.0f + Position;
+        projectile.Velocity = direction * 256.0f;
 
         projectile.HitboxComponent.TargetGroups.Add("Enemy");
 
