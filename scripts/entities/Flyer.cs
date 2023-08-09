abcc7961260a5dc3666ba1a690f8efb18d808fd2
Lasuch <stylizedstickman@gmail.com>
2023-08-09 21:37:45 +0200

    Remove HitboxComponent

--- scripts/entities/Flyer.cs
+++ scripts/entities/Flyer.cs
@@ -10,21 +10,21 @@ public partial class Flyer : CharacterBody2D
     public int ScoreOnKill = 100;
 
     [Export]
+    public Area2D Area2D;
+
+    [Export]
     public DamageComponent DamageComponent;
 
     [Export]
     public HealthComponent HealthComponent;
 
     [Export]
-    public HitboxComponent HitboxComponent;
+    public NavigationAgent2D NavigationAgent;
 
     [Export]
-    NavigationAgent2D _navigationAgent;
+    PackedScene _deathVfx;
 
-    [Export]
-    PackedScene _deathVfx = null;
-
-    Node2D _target;
+    Player _player;
 
     Game _game;
 
@@ -35,19 +35,15 @@ public partial class Flyer : CharacterBody2D
     {
         _game = GetNode<Game>("/root/Game");
 
-        _target = _game.Player;
-        _game.PlayerChanged += (player) => _target = player;
+        _player = _game.Player;
+        _game.PlayerChanged += (player) => _player = player;
 
-        HitboxComponent.TargetEntered += (target) =>
-            DamageComponent.ApplyDamage(
-                target.GetNode<HealthComponent>("HealthComponent"));
-
+        Area2D.BodyEntered += OnBodyEntered;
         HealthComponent.HealthEmpty += OnHealthEmpty;
 
-        _navigationAgent.TargetPosition = this.Position;
+        NavigationAgent.TargetPosition = Position;
 
-        _navigationAgent.VelocityComputed += (velocity) =>
-            this.Velocity = velocity;
+        NavigationAgent.VelocityComputed += (velocity) => Velocity = velocity;
     }
 
     public override void _PhysicsProcess(double delta)
@@ -57,21 +53,30 @@ public partial class Flyer : CharacterBody2D
         if (_time > 0.2)
         {
             _time = 0.0;
-            _navigationAgent.TargetPosition = _target.Position;
+            NavigationAgent.TargetPosition = _player.Position;
         }
 
-        if (_navigationAgent.AvoidanceEnabled)
+        if (NavigationAgent.AvoidanceEnabled)
         {
-            Vector2 direction = this.Position.DirectionTo(_target.Position);
-            _navigationAgent.Velocity = direction * Speed;
+            Vector2 direction = Position.DirectionTo(_player.Position);
+            NavigationAgent.Velocity = direction * Speed;
         }
         else
         {
-            Vector2 nextPathPoint = _navigationAgent.GetNextPathPosition();
-            Velocity = this.Position.DirectionTo(nextPathPoint) * Speed;
+            Vector2 nextPathPoint = NavigationAgent.GetNextPathPosition();
+            Velocity = Position.DirectionTo(nextPathPoint) * Speed;
         }
 
         MoveAndSlide();
+    }
+
+    void OnBodyEntered(Node2D body)
+    {
+        if (body is not Player)
+            return;
+
+        Player player = (Player)body;
+        DamageComponent.ApplyDamage(player.HealthComponent);
     }
 
     void OnHealthEmpty()
