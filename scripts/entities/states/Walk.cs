708f5ae44bed344ca427140551206f7e4abd8a86
Lasuch <stylizedstickman@gmail.com>
2023-08-14 18:11:20 +0200

    Add coyote time

--- scripts/entities/states/Walk.cs
+++ scripts/entities/states/Walk.cs
@@ -18,16 +18,38 @@ public partial class Walk : State
     [Export]
     State _jump;
 
+    double _fallTime = 0.0;
+
+    float Gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+
+    public override void Enter()
+    {
+        _fallTime = 0.0;
+    }
+
     public override State PhysicsProcess(double delta)
     {
         float input = Godot.Input.GetAxis("move_left", "move_right");
 
-        var velocity = new Vector2(Speed * input, 0.0f);
-        Entity.Velocity = Entity.Velocity.Lerp(velocity, Acceleration * (float)delta);
+        Vector2 velocity = Entity.Velocity;
+
+        if (!Entity.IsOnFloor())
+            velocity.Y += Gravity * (float)delta;
+
+        velocity.X = Mathf.Lerp(velocity.X, Speed * input, Acceleration * (float)delta);
+
+        Entity.Velocity = velocity;
         Entity.MoveAndSlide();
 
         if (!Entity.IsOnFloor())
-            return _fall;
+        {
+            _fallTime += delta;
+
+            if (_fallTime > 0.1)
+                return _fall;
+        }
+        else
+            _fallTime = 0.0;
 
         if (Mathf.IsZeroApprox(input))
             return _idle;
