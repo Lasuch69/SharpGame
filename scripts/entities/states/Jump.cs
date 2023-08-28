8e91095150647d225ee96a47f1870a22eb6f1b13
Lasuch <stylizedstickman@gmail.com>
2023-08-28 20:46:18 +0200

    Add variable length jump

--- scripts/entities/states/Jump.cs
+++ scripts/entities/states/Jump.cs
@@ -3,31 +3,31 @@ namespace SharpGame;
 [GlobalClass]
 public partial class Jump : State
 {
-    [Export]
-    public float JumpForce = 150.0f;
+	[Export]
+	public float JumpForce = 127.5f;
 
-    [Export]
-    State _fall;
+	[Export]
+	State _fall;
 
-    float Gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+	float Gravity = 245.0f;
 
-    public override void Enter()
-    {
-        Vector2 velocity = Entity.Velocity;
-        velocity.Y = -JumpForce;
-        Entity.Velocity = velocity;
-    }
+	public override void Enter()
+	{
+		Vector2 velocity = Entity.Velocity;
+		velocity.Y = -JumpForce;
+		Entity.Velocity = velocity;
+	}
 
-    public override State PhysicsProcess(double delta)
-    {
-        Vector2 velocity = Entity.Velocity;
-        velocity.Y += Gravity * (float)delta;
-        Entity.Velocity = velocity;
-        Entity.MoveAndSlide();
+	public override State PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Entity.Velocity;
+		velocity.Y += Gravity * (float)delta;
+		Entity.Velocity = velocity;
+		Entity.MoveAndSlide();
 
-        if (Entity.Velocity.Y > 0.0f)
-            return _fall;
+		if (!Godot.Input.IsActionPressed("jump") || Entity.Velocity.Y > 0.0f)
+			return _fall;
 
-        return null;
-    }
+		return null;
+	}
 }
